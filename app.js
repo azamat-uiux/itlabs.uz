3c8af31c2856a5f82f7ec963b2925538c3de9e59
azamat <azamatstechblog@gmail.com>
2021-12-20 02:54:03 +0500

    Fix bugs

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ form.addEventListener("submit", e => {
 	const comment = document.getElementById("comment").value
 	const select = document.getElementById("select").value
 
+	if (!firstname == "" && !lastname == "" && !phone == "" && !email == "" && !comment == "" && !select == "") {
+		const my_text = `Result is:%0A - Ism: ${firstname} %0A - Familya: ${lastname} %0A - Telefon: ${phone} %0A - Email: ${email} %0A - Izoh: ${comment} %0A - Kurs nomi: ${select}`
 
-	const my_text = `Result is:%0A - Ism: ${firstname} %0A - Familya: ${lastname} %0A - Telefon: ${phone} %0A - Email: ${email} %0A - Izoh: ${comment} %0A - Kurs nomi: ${select}`
+		const token = "5011210332:AAGVwqBLsNqvq2h0AtpXeRCAukC8Cgy2MBg"
+		const chat_id ="-607603440"
+		const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`
+	
+		console.log(firstname, lastname, phone, email, comment, select);
+	
+		let api = new XMLHttpRequest()
+	
+		api.open("GET", url, true)
+		api.send()
 
-	const token = "5011210332:AAGVwqBLsNqvq2h0AtpXeRCAukC8Cgy2MBg"
-	const chat_id ="-607603440"
-	const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`
-
-	console.log(firstname, lastname, phone, email, comment, select);
-
-	let api = new XMLHttpRequest()
-
-	api.open("GET", url, true)
-	api.send()
-
-	console.log("Xabar Jonatildi");
+		console.log("Xabar Jonatildi");
+	}
 })
